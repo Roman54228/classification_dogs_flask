c316977181847ab8b65d1f74e23b951ef9a92dc2
Roman54228 <71009563+Roman54228@users.noreply.github.com>
2022-07-28 15:10:29 +0300

    Update download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -5,5 +5,3 @@ wget https://s3.amazonaws.com/fast-ai-imageclas/imagewoof2-320.tgz
 tar -xf imagewoof2-320.tgz
 
 rm imagewoof2-320.tgz
-
-gdown https://drive.google.com/uc?id=1-0nYK87buY7c3bys9R3NWp7NPDTwzgf9
